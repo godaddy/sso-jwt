bea954c86a75ea8c0e58acb36040cb4e74d38f80
Jay Gowdy <jay@gowdy.me>
2026-04-15 12:08:53 -0700

    Use shared enclaveapp-build-support for Windows resource compilation
    Replace local build.rs boilerplate with shared compile_windows_resource().

--- sso-jwt/build.rs
+++ sso-jwt/build.rs
@@ -1,8 +1,3 @@
 fn main() {
-    #[cfg(target_os = "windows")]
-    {
-        winresource::WindowsResource::new()
-            .compile()
-            .expect("Failed to compile Windows resource");
-    }
+    enclaveapp_build_support::compile_windows_resource();
 }
